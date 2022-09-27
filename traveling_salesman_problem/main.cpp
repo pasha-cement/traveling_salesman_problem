73145c09d91cc8429fb105b44bc19534ecf1144c
pasha-cement <kolesnik-pavel-fsr@yandex.ru>
2022-09-27 16:59:47 +0300

    first commit
    Create new Xcode project.
    Add data file.

---
+++ traveling_salesman_problem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  traveling_salesman_problem
+//
+//  Created by Павел on 27.09.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
